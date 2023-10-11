aa4a6b9980fed2388ef0cfa09d5dba9654e7714a
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-10-11 12:06:29 +0500

    11.10.2023

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
-from states import Foydalanuvchi, salom
+from states import Foydalanuvchi, salom, baza
 
 
 TOKEN = "6381039530:AAEqs25byPNY7YCTV2_ed3TGAJdsUs7w_tk"
@@ -23,37 +23,53 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!\n"
                          f"Malumot olish uchun /malumotim ustiga bosing?")
 
-
-@dp.message(Command('malumotim'))
-async def malumotlar(message: types.Message, state: FSMContext) -> None:    
-   
-    await message.answer('Familyangizni kiriting')
-    await state.set_state(Foydalanuvchi.familya)        
+try:
+    @dp.message(Command('malumotim'))
+    async def malumotlar(message: types.Message, state: FSMContext) -> None:    
     
-@dp.message(Foydalanuvchi.familya)
-async def familyalar(message: types.Message, state=FSMContext) -> None:
-    familya = message.text
-    await state.update_data({
-        'familya':familya
-    })
-    await message.answer('Ismingizni kiriting')
-    await state.set_state(Foydalanuvchi.ism)
+        await message.answer('Familyangizni kiriting')
+        await state.set_state(Foydalanuvchi.familya)        
+        
+    @dp.message(Foydalanuvchi.familya)
+    async def familyalar(message: types.Message, state=FSMContext) -> None:
+        familya = message.text
+        await state.update_data({
+            'familya':familya
+        })
+        await message.answer('Ismingizni kiriting')
+        await state.set_state(Foydalanuvchi.ism)
 
-@dp.message(Foydalanuvchi.ism)
-async def ismlar(message: types.Message, state=FSMContext) -> None:
-    ism = message.text
-    await state.update_data({
-        'ism':ism
-    })
+    @dp.message(Foydalanuvchi.ism)
+    async def ismlar(message: types.Message, state=FSMContext) -> None:
+        ism = message.text        
+        await state.update_data({
+            'ism':ism
+        })
 
-    data = await state.get_data()
-    fm = data['familya']
-    im = data['ism']
-    # await message.answer(f'Familyangiz-{fm}\n'
-    #                      f'Ismingiz-{im}')
-    await message.answer(salom(fm,im))
-    await state.clear()
+        data = await state.get_data()
+        fm = data['familya']
+        im = data['ism']
+        # await message.answer(f'Familyangiz-{fm}\n'
+        #                      f'Ismingiz-{im}')
 
+        await message.answer(salom(fm,im))        
+        for dt in baza(fm,im): 
+            try:        
+            
+                await message.answer(f'Imtihon kuni: {dt[0]}\n'
+                                        f'Session id: {dt[3]}\n'
+                                        f'Total score of the test: {dt[9]}\n'
+                                        f'Speaking score: {dt[14]}')
+            except:
+                await message.answer('Bunda ma`lumot yoq')
+        
+        await state.clear()
+
+except:
+    @dp.message(CommandStart())
+    async def command_start_handler(message: Message) -> None:    
+        await message.answer(f"Hello, {hbold(message.from_user.full_name)}!\n"
+                         f"Malumot olish uchun /malumotim ustiga bosing?")
 
 async def main() -> None:
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
--- states.py
+++ states.py
@@ -11,3 +11,13 @@ def salom(fm, im):
     salomlar = f'Assolomu alaykum {fm} {im}'
     return salomlar
 
+def baza(fm, im):
+    """Bunda familya ism olib exeldan malumot qaytaradi"""
+    data = pd.read_excel('baza.xlsx')
+    df = pd.DataFrame(data)  
+    df1 = df[df['Candidate first name']== im ]
+    df2 = df1[df1['Candidate last name']== fm]
+    imtihonlar = df2.values   
+        
+    return imtihonlar
+
